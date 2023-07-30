5c87e468208001006e428b9cf57d00fbc6c80a76
Geoffrey Bii <geoffrey.bii@student.moringaschool.com>
2023-07-30 23:59:49 +0300

    creating state in bot component to hide and unhide the view of an Itemt

--- src/componenets/Bot.js
+++ src/componenets/Bot.js
@@ -45,34 +45,34 @@ function setStator(e) {
    
    }
   
-  //   if(state){
+    if(state){
 
-  //     return(
-  //       <>
+      return(
+        <>
   
-  // <div className="min-w-screen h-screen animated fadeIn faster  fixed  left-0 top-0 flex justify-center items-center inset-0 z-50 outline-none focus:outline-none bg-no-repeat bg-center bg-cover"   id="modal-id">
-  //  	<div className="absolute bg-black opacity-80 inset-0 z-0"></div>
-  //   <div className="w-full  max-w-lg p-5 relative mx-auto my-auto rounded-xl shadow-lg  bg-white ">    
-  //     <div className="">       
-  //              <div className="text-center p-5 flex-auto justify-center">
-  //                   <img src={selected.avatar_url} alt={selected.name} />
-  //                         <h2 className="text-xl font-bold py-4 ">Name: {selected.name}</h2>
-  //                         <p className="text-sm text-gray-500 px-8">Health: {selected.health}</p>  
-  //                         <p className="text-sm text-gray-500 px-8">Damage: {selected.damage}</p>  
-  //                         <p className="text-sm text-gray-500 px-8">Bot Class: {selected.bot_class}</p>
-  //                         <p className="text-sm text-gray-500 px-8">catchphrase: {selected.catchphrase}</p>
-  //                    </div>
-  //      <div className="p-3  mt-2 text-center space-x-4 md:block">
-  //         <button onClick={setStator} className="mb-2 md:mb-0 bg-white px-5 py-2 text-sm shadow-sm font-medium tracking-wider border text-gray-600 rounded-full hover:shadow-lg hover:bg-gray-100">Close</button> 
-  //            </div>
-  //       </div>
-  //     </div>
-  //   </div>
+  <div className="min-w-screen h-screen animated fadeIn faster  fixed  left-0 top-0 flex justify-center items-center inset-0 z-50 outline-none focus:outline-none bg-no-repeat bg-center bg-cover"   id="modal-id">
+   	<div className="absolute bg-black opacity-80 inset-0 z-0"></div>
+    <div className="w-full  max-w-lg p-5 relative mx-auto my-auto rounded-xl shadow-lg  bg-white ">    
+      <div className="">       
+               <div className="text-center p-5 flex-auto justify-center">
+                    <img src={selected.avatar_url} alt={selected.name} />
+                          <h2 className="text-xl font-bold py-4 ">Name: {selected.name}</h2>
+                          <p className="text-sm text-gray-500 px-8">Health: {selected.health}</p>  
+                          <p className="text-sm text-gray-500 px-8">Damage: {selected.damage}</p>  
+                          <p className="text-sm text-gray-500 px-8">Bot Class: {selected.bot_class}</p>
+                          <p className="text-sm text-gray-500 px-8">catchphrase: {selected.catchphrase}</p>
+                     </div>
+       <div className="p-3  mt-2 text-center space-x-4 md:block">
+          <button onClick={setStator} className="mb-2 md:mb-0 bg-white px-5 py-2 text-sm shadow-sm font-medium tracking-wider border text-gray-600 rounded-full hover:shadow-lg hover:bg-gray-100">Close</button> 
+             </div>
+        </div>
+      </div>
+    </div>
        
-  //       </>
-  //     )
-  //    }
-  //    else{
+        </>
+      )
+     }
+     else{
 
     return (
       
