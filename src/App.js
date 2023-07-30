fdaed44f044b610ac9f44ee9f06d0ca4e71df99a
Geoffrey Bii <geoffrey.bii@student.moringaschool.com>
2023-07-30 23:43:49 +0300

    structuring arm bot project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import BotList from './componenets/BotList';
+import Search from './componenets/Search';
 import './App.css';
+import BotArmy from './componenets/BotArmy';
+
+
+
+
 
 function App() {
+ 
+  const [theme, setTheme] = useState("light")
+  const [query, setQuery] = useState("")
+  const [bots, setBots] = useState([])
+  const [army, setArmy] = useState([]);
+  
+
+  useEffect(()=>{
+   fetch(" http://localhost:3000/bots?q="+ query)
+   .then(r=>r.json())
+   .then(data=>setBots(data))  
+  },[query])
+
+
+
+function handleSearch(e) {
+    setQuery(e.target.value)
+  }
+
+ function handleTheme(e) {
+  e.preventDefault()
+  if(theme=== "light"){setTheme("dark")}
+  else{
+    setTheme("light")
+  }
+
+  
+ }
+ useEffect(()=>{
+document.body.className =theme;
+
+ }, [theme])
+
+ function onDeleteBots(id){
+  const updateBots = bots.filter(transa=>
+    transa.id !==id      
+  )
+  setBots(updateBots)
+
+}
+
+
+ const addBotToArmy = (bot) => {
+   if (!army.some((b) => b.id === bot.id || b.bot_class===bot.bot_class)) {
+     setArmy([...army, bot]);
+     alert("Hello! You have added successfully!");
+   }
+   else{
+    alert("Already exist or you are trying to add bot from the same class!")
+   }
+ };
+
+ const releaseBot = (botId) => {
+   const updatedArmy = army.filter((bot) => bot.id !== botId);
+   setArmy(updatedArmy);
+ };
+
+ const dischargeBot = (botId) => {
+   const updatedArmy = army.filter((bot) => bot.id !== botId);
+   setArmy(updatedArmy);
+
+  
+ };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className={`App ${theme}`}>
+
+     
+    <button onClick={handleTheme}>Dark</button>
+       <Search handleSearch={handleSearch} />
+       
+       <BotArmy army={army}  releaseBot={releaseBot} dischargeBot={dischargeBot} />
+       <BotList bots={bots} onDeleteBots ={onDeleteBots} addBotToArmy={addBotToArmy} />
+     
+     
+   
     </div>
+   
   );
 }
 
